6a11fa86c6496d8ea06be6d95e4ce2c572547408
SlavaChebotaryov <frerfhzxf@gmail.com>
2023-09-04 15:54:34 +0300

    Добавить реализацию репозитория книг в памяти

---
+++ Store.Memory/BookRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.Memory
+{
+	public class BookRepository : IBookRepository
+	{
+		private readonly Book[] books = new[]
+		{
+			new Book(1, "art"),
+			new Book(2, "Ref"),
+			new Book(3, "C Prog")
+		};
+
+		public Book[] GetAllByTitle(string titlePart) => books.Where(b => b.Title.Contains(titlePart, StringComparison.InvariantCultureIgnoreCase)).ToArray();
+	}
+}
